564a018f27d19803d769a36a15e38d9a6113a427
Alisson Silva <alissonjosef@gmail.com>
2022-06-15 10:33:58 -0300

    generator qr code

---
+++ pages/QRCodeCanvas.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import QRCode from 'qrcode'
+
+interface Props {
+    text: string
+}
+
+function QRCodeCanvas ({text}: Props)  {
+
+    const canvasRef = useRef()
+
+    useEffect(() => {
+        QRCode.toCanvas(canvasRef.current, text,{width:500},(error) => {
+            console.log(error)
+        })
+    }, [text]);
+
+  return (
+    <>
+        <canvas ref={canvasRef} id='canvas'></canvas>
+    </>
+  )
+}
+
+export default QRCodeCanvas;
